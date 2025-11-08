0248ca07e48c2145b87fdf33fb211595b42b01e6
verbose <ev3rnub@gmail.com>
2025-11-08 10:19:45 -0700

    Removed some non working python code, updated comments across main, updated .txt help files to include src info, updated readme.md

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 # Chad Verbus
 # CSC325 - Python
-# 10292025
+# 11082025
 
+
+
+
+# interact os libs so we can clear the terminal screen etc.
 import os
-import re
-# future
-def init():
-    pass
+
 #main loop
 def main():
     isrunning: bool = True
     while isrunning:
-        getInput = input("Enter a keyword, followed by the enter key:")
+        getInput = input(blue_output("Enter a keyword, followed by the enter key:"))
         if getInput == "exit" or getInput == "q" or getInput == "quit":
             help("goodbye")
             isrunning = False
         else:
             help(getInput)
 
+# helper function
 def help(name):
     match name:
         case "goodbye":
-            print(white_output("VPHS is shutting down. Goodbye!"))
+            print(blue_output("VPHS is shutting down. Goodbye!"))
         case "information":
-            print("Sample CLI APP for CSC325: Prof Hinton: Assignment .5.1: Your First Python Application: Verbose Python Help System: Chad Verbus")
+            print(blue_output("Sample CLI APP for CSC325: Prof Hinton: Assignment .5.1: Your First Python Application: Verbose Python Help System: Chad Verbus"))
         case "help":
             display_welcome_message()
         case "q":
@@ -35,7 +37,7 @@ def help(name):
         case _:
             read_help_text(name)
 
-
+# match aType and print help text accordingly.
 def read_help_text(aType):
     match aType:
         case "def":
@@ -57,69 +59,72 @@ def read_help_text(aType):
         case _:
            print(blue_output("For help, type 'help', else quit, or type q followed by enter to exit."))
 
-
+# limit the amount of lines printed at one time for easier reading.
 def output(someFile):
+    # define variable of type in and initilize it to 0.
     cnt :int = 0
+    # open some file and push it into a list of lines.
     with open(someFile, "r") as file:
         lines = file.readlines()
+    # for each line, print to terminal.
     for line in lines:
         #regex func goes here
-        print(green_output(parse_titles(line)))
+        print(white_output(line))
+        # increment cnt
         cnt += 1
+        # if cnt is 17, then ask user to press enter to continue to print next 17 lines.
         if cnt == 17:
-            getInput = input(gray_output("Press enter to continue or type 'q' to quit:"))
+            getInput = input(white_output("Press enter to continue or type 'q' to quit:"))
             if getInput == 'q' or getInput == 'quit' or getInput == 'exit':
                 break
             else:
                 cnt = 0
 
-# NOT working
-def parse_titles(someLine):
-    title_regex = r"\*\*.*\*\*"
-    title_match = re.search(title_regex, someLine)
-    if title_match:
-        return bold_output(gray_output(someLine))
-    else:
-        return someLine
 
+ # colorize terminal output
 def green_output(someData) -> str:
     GREEN = "\x1b[32m"
     processedData = GREEN + someData + GREEN
     return processedData
-
+# red
+def red_output(someData) -> str:
+    RED = "\x1b[31m"
+    processedData = RED + someData + RED
+    return processedData
+# white
 def white_output(someData) -> str:
-    WHITE = "\x1b[37m"
+    WHITE = "\x1b[38;2;255;255;255m"
     processedData = WHITE + someData + WHITE
     return processedData
-
+# gray
 def gray_output(someData) -> str:
     GRAY = "\x1b[37m"
     processedData = GRAY + someData + GRAY
     return processedData
-
+#bold
 def bold_output(someData) -> str:
     BOLD = "\x1b[1m"
     processedData = BOLD + someData + BOLD
     return processedData
-
+#blue
 def blue_output(someData) -> str:
     BLUE = "\x1b[34m"
     processedData = BLUE + someData + BLUE
     return processedData
-
+# reset
 def reset_output(someData) -> str:
     RESET = "\x1b[0m"
     processedData = RESET + someData + RESET
     return processedData
 
-
+# welcome message dialog
 def display_welcome_message():
     os.system("clear")
-    print("Welcome to Verbose Python Help System (VPHS).")
-    print("This is the early initial stages of development. Please report any bugs or issues to Chad Verbus.")
-    print("You can type key words followed by the enter key, the system will display simple text related to the keyword")
-    print("\n")
-    print("Working Key Words:\n")
+    print(green_output("Welcome to Verbose Python Help System (VPHS)."))
+    print(white_output("This is the early initial stages of development. Please report any bugs or issues to Chad."))
+    print(white_output("You can type key words followed by the enter key, the system will display simple text related to the keyword"))
+    print("\n\n")
+    print(bold_output(white_output("Working Key Words:\n")))
     print("- help - displays help information")
     print("- python - what is python?")
     print("- def - functions and classes in python")
@@ -131,10 +136,7 @@ def display_welcome_message():
     print("- variables - variables in python")
     print("\n\n\n")
 
-# Press the green button in the gutter to run the script.
+# if our python script is run directly, then run main()
 if __name__ == '__main__':
     display_welcome_message()
     main()
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 Course: CSC235
 Author: Chad Verbus
-Date: Nov 01, 2025
+Date: Nov 08,2025
 
 
 ⚠️ Working Document – This README is a living document that will be updated throughout the semester as new topics, assignments, and resources become available.  
@@ -9,3 +9,10 @@ Date: Nov 01, 2025
 * 1. Created simple loop to monitor input from user, process said input and display "help" text regarding python.
 * 2. Added generic color scheme to output.
 * 3. Updated Readme.md.
+
+## 11/08/2025
+* 1. Removed non working code.
+* 2. Updated comments throughout main.
+* 3. Updated white_output func, to use a brighter white color.
+* 4. Updated .txt files with additional source information.
+* 5. Updated Readme.md.
